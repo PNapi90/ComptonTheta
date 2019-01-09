7e8aa0670fb794d52962f641e3420d03f6745a19
Philipp <p.napiralla13@googlemail.com>
2019-01-09 13:19:30 +0100

    again

--- Main.cpp
+++ Main.cpp
@@ -12,7 +12,7 @@ int main()
     std::vector<std::shared_ptr<Processor> > P;
     P.reserve(nthr);
 
-    for(int i = 0;i < 40;++i) P.push_back(std::make_shared<Processor>(i*nthr,fileAmount));
+    for(int i = 0;i < 40;++i) P.push_back(std::make_shared<Processor>(i*fileAmount,fileAmount));
 
     std::thread t[nthr];
     for(int i = 0;i < nthr;++i) t[i] = P[i]->threading();
