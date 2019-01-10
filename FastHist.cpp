2de17d4621ff9e0976327e3030577c2112e0cb2c
Philipp <p.napiralla13@googlemail.com>
2019-01-10 09:44:52 +0100

    Hists Made

--- FastHist.cpp
+++ FastHist.cpp
@@ -32,31 +32,47 @@ bool FillHistogram(std::vector<double> &Values, std::vector<std::vector<double> 
 
 int main()
 {
+    std::string line,name;
+    std::ifstream DATA;
 
-    std::string line;
-    std::ifstream DATA("Processed.dat");
+    std::vector<double> Values(2, 0);
+    std::vector<std::vector<double>> Hist(350, std::vector<double>(180, 0));
 
-    std::vector<double> Values(2,0);
-    std::vector<std::vector<double> > Hist(350,std::vector<double>(180,0));
-    
     bool tmpBool = true;
 
-    while(std::getline(DATA,line))
+    for (int i = 0; i < 10; ++i)
     {
-        std::istringstream Buffer(line);
-        std::vector<std::string> Words(std::istream_iterator<std::string>{Buffer},std::istream_iterator<std::string>());
+        name = "ProcessedData/Processed" + std::to_string(i * 100) + ".dat";
+        DATA.open(name);
 
-        for(int i = 0;i < 2;++i)
+        if(DATA.fail())
         {
-            Values[i] = std::stod(Words[i]);
+            std::cerr << "Could not find " << name << std::endl;
+            return 1;
         }
 
-        if(Values[1] != Values[1]) continue;
+        while (std::getline(DATA, line))
+        {
+            std::istringstream Buffer(line);
+            std::vector<std::string> Words(std::istream_iterator<std::string>{Buffer}, std::istream_iterator<std::string>());
 
-        tmpBool = FillHistogram(Values,Hist);
-        if(!tmpBool) return 1;
+            for (int i = 0; i < 2; ++i)
+            {
+                Values[i] = std::stod(Words[i]);
+            }
 
+            if (Values[1] != Values[1])
+                continue;
+
+            tmpBool = FillHistogram(Values, Hist);
+            if (!tmpBool)
+                return 1;
+        }
+        DATA.close();
+        DATA.clear();
+        std::cout << "File " << i*100 << " done" << std::endl;
     }
+
 
     std::cout << "Normalizing histograms ...";
     std::cout.flush();
@@ -67,25 +83,19 @@ int main()
         for (auto Val : Hist[i]) Norms[i] += Val;
     }
 
+    std::cout << " done" << std::endl;
+
     std::cout << "Saving histogram ... ";
     std::cout.flush();
 
     std::ofstream OUT("HistsOut.dat");
     for (int i = 0; i < 350; ++i)
     {
-        if(Norms[i] > 0)
-        {
-            for (auto Val : Hist[i]) OUT << Val/Norms[i] << " ";
-            OUT << std::endl;
-        }
-        
-        else
-        {
-            for (auto Val : Hist[i])
-                OUT << Val << " ";
-            OUT << std::endl;
-        }
-        
+
+        Norms[i] = Norms[i] > 0 ? Norms[i] : 1.;
+
+        for (auto Val : Hist[i]) OUT << Val/Norms[i] << " ";
+        OUT << std::endl;        
     }
 
     std::cout << "done" << std::endl;
--- plotter.py
+++ plotter.py
@@ -2,6 +2,10 @@ import numpy as np
 import pylab as py
 import math
 
+from matplotlib import rc
+rc('text', usetex=True)
+
+
 py.close('all')
 
 from matplotlib.colors import LogNorm
@@ -17,12 +21,17 @@ py.clf()
 for i in range(len(data)-1):
     if i % 10 != 0 or i*2 > 500:
         continue
-    py.plot(angles,data[i,:],color=colors[i],ls=":",marker='o',lw=2,ms=4,label=str(i*2))
+    py.plot(angles,data[i,:],color=colors[i],ls=":",marker='o',lw=1.5,ms=3)#,label=str(i*2))
     #py.axvline(i*2,color=colors[i],ls=':')
-py.legend(loc=2,fontsize=8)
+py.tick_params(axis='both', which='major', labelsize=15)
+py.tick_params(axis='both', which='minor', labelsize=10)
+py.xlabel("$\\theta$ in deg",fontsize=20)
+py.ylabel("Probability Density",fontsize = 20)
+
+py.savefig("sampled.pdf",bbox_inches="tight")
 
 
-"""
+data2 = np.loadtxt("Processed.dat")
 
 
 Eg = 661.7
@@ -38,18 +47,23 @@ Edeps = np.linspace(0,Edge,1000)
 
 angles = [angle(E) for E in Edeps]
 
-py.figure(1,figsize=(5,4))
+py.figure(2,figsize=(5,4))
 py.clf()
-py.hist2d(data[:,0],data[:,1],range=[[0,700],[0,180]],bins=(350,180),cmap = py.cm.rainbow,cmin = 1)
-py.colorbar()
-py.plot(Edeps,angles,color="k",lw=1)
+py.hist2d(data2[:,0],data2[:,1],range=[[0,700],[0,180]],bins=(350,180),cmap = py.cm.jet,cmin = 1)
+py.tick_params(axis='both', which='major', labelsize=15)
+py.tick_params(axis='both', which='minor', labelsize=10)
+py.ylabel("$\\theta$ in deg",fontsize=20)
+py.xlabel("$E_{dep}$ in keV",fontsize = 20)
+#py.plot(Edeps,angles,color="k",lw=1)
+py.savefig("sampled2.pdf",bbox_inches="tight")
 
+"""
 iter = 0
-anglesSave = np.zeros(len(data))
+anglesSave = np.zeros(len(data2))
 
-for i in range(len(data)):
-    if data[i,0] >= Edge:
-        anglesSave[iter] = data[i,1]
+for i in range(len(data2)):
+    if data2[i,0] >= Edge:
+        anglesSave[iter] = data2[i,1]
         iter += 1
 
 py.figure(2,figsize=(5,4))
